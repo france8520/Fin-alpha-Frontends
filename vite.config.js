11af5e19ddd09d912378d6903728a3187261132d
france8520 <thananoon@gmail.com>
2026-06-27 20:04:51 +0700

    vercel-ai-api

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["sb-5glu418z8mhl.vercel.run"]
+  }
 })
