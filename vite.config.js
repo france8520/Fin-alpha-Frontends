0de7cef778faf8634d2c4caa23e3b3aff401c016
france8520 <thananoon@gmail.com>
2026-06-28 08:53:58 +0700

    Vite fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["sb-5glu418z8mhl.vercel.run"]
+    allowedHosts: true
   }
 })
